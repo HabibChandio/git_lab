b128e7c4a6e65ade80e23b026db0ee94240331d4
HabibChandio <rehman4704616@cloud.neduet.edu.pk>
2024-11-13 18:58:27 +0500

    Initialzed project with main.c and a readme file

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    float n1, n2, result;
+    char op;
+    printf("Instructions: Enter Q when asked for operator to quit.\n");
+    do{
+        //body
+    }while(op != 'Q');
+    
+    return 0;
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+This is a documentation of the "Calculator Project"
+
+GIT commands used:
+
+1. git init - to initialize repository
